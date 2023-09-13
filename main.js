04ea67921a346eee38edf235fcc6f31e22e04ad6
janlurenceespinosa <144762683+janlurenceespinosa@users.noreply.github.com>
2023-09-13 11:31:49 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const main = document.querySelector('.main');
+const cancel = document.querySelector('.cancel');
+const msg = document.querySelector('#msg');
+
+while (true) {
+        alert("What if...");
+        alert("what if may chance kayo ni shierel");
+        alert('What if kamo jud sa usag usa ');
+        alert('ingon dayon ug I love YOu kier yawaa nmo uyy ka weak hahaha ');
+        main.style.display = 'block';
+        break;
+    
+}
